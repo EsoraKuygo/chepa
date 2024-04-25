3cec80b79834e71f05dbda2cee6926d653f910e0
EsoraKyugo <tom.7trouillet@gmail.com>
2024-04-25 11:23:28 +0200

    click

---
+++ data/script.js
@@ -0,0 +1,7 @@
+// Sélectionner le bouton
+var bouton = document.getElementById("monBouton");
+
+// Ajouter un écouteur d'événement pour le clic
+bouton.addEventListener("click", function() {
+    alert("Vous avez cliqué sur le bouton !");
+});
