746aade37f9ea2cdbca8a79cfe8c4c54328c8033
EsoraKyugo <tom.7trouillet@gmail.com>
2024-04-25 11:25:35 +0200

    jout de l'image

--- data/script.js
+++ data/script.js
@@ -1,7 +1,6 @@
-// Sélectionner le bouton
+
 var bouton = document.getElementById("monBouton");
 
-// Ajouter un écouteur d'événement pour le clic
 bouton.addEventListener("click", function() {
     alert("Vous avez cliqué sur le bouton !");
 });
